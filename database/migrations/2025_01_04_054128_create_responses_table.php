12fb83eccb574bdc07a075ffadea87070ed72a6e
remove <tronton331@gmail.com>
2025-01-04 20:06:48 +0700

    First Install and Configuration

---
+++ database/migrations/2025_01_04_054128_create_responses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('form_id');
+            $table->unsignedBigInteger('user_id');
+            $table->dateTime('date');
+
+            $table->foreign('form_id')->references('id')->on('forms');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('responses');
+    }
+};
---
+++ database/migrations/2025_01_04_093323_create_answers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('response_id');
+            $table->unsignedBigInteger('question_id');
+            $table->text('value');
+            
+            $table->foreign('response_id')->references('id')->on('responses');
+            $table->foreign('question_id')->references('id')->on('questions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+};
